f664f382c22c4947c15b5402f424dfc0b4d927d6
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 11:12:12 +0200

    display latest Post

--- blobProjectDjango/urls.py
+++ blobProjectDjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url,include
 urlpatterns = [
+    url(r'^$',include('posts.urls')),
     url(r'^admin/',admin.site.urls),
     url(r'^posts/',include('posts.urls'))
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import Posts
 def index(request):
     # return HttpResponse('Hello From Post Router')
-    return render(request,'posts/index.html',{
-        'title':'Latest Posts'
-    })
+
+    posts=Posts.objects.all()[:10]
+
+    context={
+         'title':'Latest Posts',
+         'posts':posts
+    }
+    return render(request,'posts/index.html',context)
