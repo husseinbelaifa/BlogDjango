cdd09ee6d26778da3fece66d13a2895f676d3b17
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 14:31:39 +0200

    remove filed create_at & author from admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Posts
 # Register your models here.
-admin.site.register(Posts)
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('author','created_at')
+
+admin.site.register(Posts,PostAdmin)
