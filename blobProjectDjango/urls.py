8c3e130976873f007a1e612009edde07b46c4405
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 10:08:32 +0200

    set up url for post

--- blobProjectDjango/urls.py
+++ blobProjectDjango/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
+from django.conf.urls import url,include
 urlpatterns = [
-    url(r'^admin/',admin.site.urls)
+    url(r'^admin/',admin.site.urls),
+    url(r'^posts/',include('posts.url'))
 ]
