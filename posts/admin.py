b5ad51a41258780170e07cd844a319e2933196bb
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 14:39:52 +0200

    display authorName

--- posts/admin.py
+++ posts/admin.py
@@ -4,4 +4,10 @@ from .models import Posts
 class PostAdmin(admin.ModelAdmin):
     exclude = ('author','created_at')
 
+    def save_model(self, request, obj, form, change):
+        obj.author=request.user
+        obj.save()
+        # return super().save_model(request, obj, form, change)
+
+    
 admin.site.register(Posts,PostAdmin)
