efe42b233831e421f2f0f5e60a4bb4a59831b3cf
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 10:49:29 +0200

    migrate posts

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime 
 # Create your models here.
 
-class Post(models.Model):
+class Posts(models.Model):
     title=models.CharField(max_length=200)
     body=models.TextField()
     created_at=models.DateTimeField(default=datetime.now(),blank=True)
