f75568eb6571fd161d4fbb5a8b5232670d267c70
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 10:33:14 +0200

    set up layout.html

--- posts/views.py
+++ posts/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello From Post Router')
+    # return HttpResponse('Hello From Post Router')
+    return render(request,'posts/index.html')
