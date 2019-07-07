54c5d6c2ad6db5f4379c76b4b4006b048dd9eeae
belaifa el hussein <belaifahocine@gmail.com>
2019-07-07 10:16:56 +0200

    config post router & display response

--- blobProjectDjango/urls.py
+++ blobProjectDjango/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.conf.urls import url,include
 urlpatterns = [
     url(r'^admin/',admin.site.urls),
-    url(r'^posts/',include('posts.url'))
+    url(r'^posts/',include('posts.urls'))
 ]
